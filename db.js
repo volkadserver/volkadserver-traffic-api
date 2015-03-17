2ef5082dfce649a58bac955c32431367912611d1
Nathan Hagen <nathan@aggronn.com>
2015-03-16 22:24:51 -0400

    Add basic models for sequelize

--- db.js
+++ db.js
@@ -1,6 +1,34 @@
-var Sequelize = require('sequelize');
+var S = require('sequelize');
 
-var db = new Sequelize('volk', 'volk', 'password');
+var db = new S('volk', 'volk', 'password');
+
+var User = db.define('user', {
+    firstName: { type: S.STRING },
+    lastName: { type: S.STRING }
+  }
+);
+
+var Flight = db.define('flight', { 
+  flightName: S.STRING,
+  startDate: S.DATE,
+  endDate: S.DATE
+});
+
+var Campaign = db.define('campaign', { 
+  campaignName: S.STRING,
+  startDate: S.DATE,
+  endDate: S.DATE
+});
+
+var Creative = db.define('creative', { 
+  creativeName: S.STRING,
+});
+
+
+var Advertiser = db.define('advertiser', { 
+  advertiserName: S.STRING,
+}).sync({ force: true });
+
 
 
 module.exports = db;
