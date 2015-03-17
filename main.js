ee67067444d35cc1e1d07aa3d2918ddc99895cea
Nathan Hagen <nathan@aggronn.com>
2015-03-16 22:24:22 -0400

    Add koa-logger

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var koa = require('koa');
 var app = koa();
+var logger = require('koa-logger');
 var router = require('./router');
 var db = require('./db');
 
+
 app
+  .use(logger())
   .use(router.routes())
   .use(router.allowedMethods());
 
