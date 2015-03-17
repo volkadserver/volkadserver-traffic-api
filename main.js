4ff55e07da786a3b7fb403829adf79eba86e9e23
Nathan Hagen <nathan@aggronn.com>
2015-03-16 23:41:47 -0400

    Add koa-cors

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 var koa = require('koa');
 var app = koa();
 var logger = require('koa-logger');
+var cors = require('koa-cors');
 var router = require('./router');
 var db = require('./db');
 
 
 app
   .use(logger())
+  .use(cors())
   .use(router.routes())
   .use(router.allowedMethods());
 
