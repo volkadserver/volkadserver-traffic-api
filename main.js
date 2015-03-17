ac0958121ed3cbbbeea80b7b0488d62f2bb78c3e
Nathan Hagen <nathan@aggronn.com>
2015-03-17 14:28:28 -0400

    Add koa-body parser for POST data

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var koa = require('koa');
 var app = koa();
 var logger = require('koa-logger');
+var bodyParser = require('koa-body');
 var cors = require('koa-cors');
 var router = require('./router');
 var db = require('./db');
@@ -8,6 +9,7 @@ var db = require('./db');
 
 app
   .use(logger())
+  .use(bodyParser())
   .use(cors())
   .use(router.routes())
   .use(router.allowedMethods());
